13a9c350491ba56370948f1e8b35cf508df33b3d
Liam <liam205@outlook.com>
2016-12-13 12:16:04 +1100

    Added snippets to results returned

--- client.js
+++ client.js
@@ -2,37 +2,36 @@ $(function() {
   
   
   document.querySelector("#searchBtn").addEventListener("click",function(){
-    var searchInput = document.querySelector("#searchInput");
-    console.log(searchInput.value);
-    
-    if(searchInput && searchInput.value){
-      var ourRequest = new XMLHttpRequest();
-    
-      ourRequest.open('GET','https://en.wikipedia.org/w/api.php?action=query&format=json&indexpageids=1&prop=info&generator=search&inprop=url&origin=*&gsrsearch='+searchInput.value);
-      ourRequest.onload= function(){
-      var ourFile = JSON.parse(ourRequest.responseText);
-      for(i=0;i<ourFile.query.pageids.length;i++){
-        var pageId= ourFile.query.pageids[i];
-        var ourUrl = ourFile.query.pages[pageId].fullurl;
-        //console.log(searchInput);
-        console.log(pageId);
-        console.log(ourUrl);
-        createHtml(ourFile);
-        }
-      };
-  ourRequest.send();
-    }
-    else{
-      console.log("User needs to give some input to search");
-    }
-});// closes anonymous function of event clicking the button
+ var searchInput = document.querySelector("#searchInput");
+ var searchInputValue = searchInput.value;
+  document.querySelector("#results").innerHTML ="";
+ //if (searchInput && searchInputValue)
+ var ourRequest = new XMLHttpRequest();
+ ourRequest.open('GET','https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&srsearch='+searchInputValue);
+ ourRequest.onload=function(){
+   var ourData = JSON.parse(ourRequest.responseText);
+   
 
-function createHtml(UrlData){
-  var rawTemplate = document.querySelector("#urlTemplate");
-  var compiledTemplate = Handlebars.compile(rawTemplate);
-  var ourGeneratedHtml = compiledTemplate(urlData);
-  
-  var urlContainer = document.querySelector("#url-container");
-  urlContainer.innerHtml = ourGeneratedHtml;
-}
+   //first do a for loop then delete it and use Handlebars
+   for(i=0;i<ourData.query.search.length;i++){
+     var title = ourData.query.search[i].title;
+     var snippet = ourData.query.search[i].snippet;
+     console.log(title);
+     document.querySelector("#results").innerHTML += '<h2><a href="http://en.wikipedia.org/wiki/'+title+'" target=_blank>'+title+'</a></h2>'+snippet;
+   }
+   //createHtml(ourData);
+ }
+ourRequest.send();
+});// closes anonymous function of event clicking the button
 }) ;
+
+
+// HandleBars tutorial
+//function createHtml(UrlData){
+ // var rawTemplate = document.querySelector("#urlTemplate");
+  //var compiledTemplate = Handlebars.compile(rawTemplate);
+ // var ourGeneratedHtml = compiledTemplate(urlData);
+  
+ // var urlContainer = document.querySelector("#url-container");
+  //urlContainer.innerHtml = ourGeneratedHtml;
+//}
